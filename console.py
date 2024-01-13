38148575db1d30f540aa61f2e1e443fa64455ac7
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-13 15:23:33 +0200

    Update console.py

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Console module"""
 import cmd
-from models import storage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
