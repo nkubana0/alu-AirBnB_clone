1864879bbc235e2f933526cdb478a0d256cd2e19
nkubana0 <i.shema@alustudent.com>
2024-01-14 07:44:53 -0800

    test_base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """BaseModel module"""
-
 import uuid
 from datetime import datetime
-import models
-
 
 class BaseModel:
     """BaseModel class"""
@@ -16,31 +13,35 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 elif key != '__class__':
                     setattr(self, key, value)
-            if 'id' not in kwargs:
-                setattr(self, 'id', str(uuid.uuid4()))
-            time = datetime.now()
-            if 'created_at' not in kwargs:
-                setattr(self, 'created_at', time)
-            if 'updated_at' not in kwargs:
-                setattr(self, 'updated_at', time)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            return
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """String representation of BaseModel"""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, str(self.__dict__))
+        """String representation method"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at attribute with current datetime"""
+        """Save method"""
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
-        """Return dictionary representation of BaseModel"""
-        new_dict = dict(self.__dict__)
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        """Dictionary representation method"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+if __name__ == '__main__':
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
