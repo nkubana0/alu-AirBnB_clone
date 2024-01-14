5bf56ccc5c97f776c5ba642b0ffc95d784a9bc25
nkubana0 <i.shema@alustudent.com>
2024-01-14 07:53:54 -0800

    test_base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,23 @@
 #!/usr/bin/python3
-"""BaseModel module"""
+
 import uuid
 from datetime import datetime
 
 class BaseModel:
-    """BaseModel class"""
-    def __init__(self, *args, **kwargs):
-        """Initialization method"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
-                    setattr(self, key, value)
-            return
+    def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation method"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Save method"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Dictionary representation method"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-if __name__ == '__main__':
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
