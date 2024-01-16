591087c34528960aa9fe3a875d3383649f52c085
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-17 01:02:18 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            FileStorage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
