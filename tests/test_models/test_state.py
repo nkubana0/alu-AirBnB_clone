cdeef1fe04a5b362fb43b48cd3da0cd13d247a0d
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2024-01-18 15:35:05 +0200

    unittest

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,1 +1,35 @@
- 
+'''Test file for the 'State' class definition.
+'''
+import unittest
+from models.base_model import BaseModel
+from models.state import State
+
+
+class TestStateClass(unittest.TestCase):
+    ''''TestStateClass' class. Used to test
+    the definition of the 'State' class.
+    '''
+    def setUp(self):
+        '''Sets up variables for use in later parts of the test.
+        '''
+        self.test_state_instance = State()
+        self.test_state_instance.save()
+
+    def test_inherits_from_BaseModel(self):
+        '''Test if the instance of class 'State' inherits from 'BaseModel'.
+        '''
+        self.assertIsInstance(self.test_state_instance, BaseModel)
+
+    def test_has_name_attribute(self):
+        '''Test if the 'State' class has the 'name' attribute.
+        '''
+        self.assertTrue(hasattr(State, "name"))
+
+    def test_name_attribute_type_str(self):
+        '''Tests the type of the 'name' attribute in the 'State' class.
+        '''
+        self.assertIsInstance(State.name, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
