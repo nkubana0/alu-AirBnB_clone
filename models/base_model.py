9ad73513b3f458eb312ad3c97c0f4c48b4bdaf18
Eliane_M <munezeroeliane761@gmail.com>
2024-01-18 13:55:05 -0800

    #3

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,40 @@
 #!/usr/bin/python3
+"""
+defines all common attributes for other classes
+"""
 
 import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+    def __init__(self, id, created_at, updated_at):
+        """
+        initialising
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current datetime
+        updates updated_at to ccurrent time
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance
+        returns a dictionary containing all key values
         """
-        __class__ : self.__class__.__name__
+        __class__ :self.__class__.__name__
         dict_representation : self.__dict__.copy()
-        dict_representation['__class__'] : class_name
-        dict_representation['created_at'] : self.created_at.isoformat()
+        dict_representation['__clas__'] : class_name
+        dict_representation['created_at'] : self.created.isoformat()
         dict_representation['updated_at'] : self.updated_at.isoformat()
-        return dict_representation
+        return dict_represenation
 
     def __str__(self):
         """
-        Returns a string representation of the instance
+        returns a string representation of the instance
         """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        class_name= self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name,self.id, self.__dict__
