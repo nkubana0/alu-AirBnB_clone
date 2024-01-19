196205ccf8d498bbcc6648546f763abe981bc59e
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 22:28:54 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,6 @@
 
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.review import Review
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
 
 classes = {
     'BaseModel': BaseModel,  # Add other classes as needed
