377b6174d026d663a6fb1246f6634629829bd47e
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 14:45:50 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,43 +5,35 @@ from models.base_model import BaseModel
 from os import path
 
 class FileStorage:
+    """Serializes instances to a JSON file and deserializes JSON file to instances"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Retrieve all stored objects.
-        Returns:
-            dict: A dictionary containing all stored objects.
-        """
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """
-        Add a new object to storage.
-        Args:
-            obj: The object to be added to storage.
-        """
+        """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """
-        Save serialized objects to a JSON file.
-        """
+        """Serializes __objects to the JSON file (path: __file_path)"""
         obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(obj_dict, file)
 
     def reload(self):
-        """
-        Reload objects from a JSON file.
-        """
-        if path.exists(self.__file_path):
+        """Deserializes the JSON file to __objects"""
+        try:
             with open(self.__file_path, 'r', encoding='utf-8') as file:
                 obj_dict = json.load(file)
-                for key, obj_data in obj_dict.items():
+                for key, value in obj_dict.items():
                     class_name, obj_id = key.split('.')
-                    class_obj = globals()[class_name]
-                    obj_instance = class_obj(**obj_data)
+                    class_module = __import__('models.' + class_name, fromlist=[class_name])
+                    obj_instance = getattr(class_module, class_name)(**value)
                     self.__objects[key] = obj_instance
+        except FileNotFoundError:
+            pass
