b88f117606ff8a64fb91a9bd611ba1f90a6aa738
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 21:11:04 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,43 +1,39 @@
 #!/usr/bin/python3
 
 import json
-import os
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        """Serializes __objects to the JSON file."""
         data = {}
         for key, obj in self.__objects.items():
             data[key] = obj.to_dict()
-
-        # Exclude FileStorage instance from data
-        if 'FileStorage.' + str(id(self)) in data:
-            del data['FileStorage.' + str(id(self))]
-
         with open(self.__file_path, 'w') as file:
             json.dump(data, file)
 
     def reload(self):
+        """Deserializes the JSON file to __objects."""
         try:
             with open(self.__file_path, 'r') as file:
                 data = json.load(file)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
+                    # Import BaseModel here to avoid circular import
+                    from models.base_model import BaseModel
                     cls = globals()[class_name]
                     obj = cls(**value)
                     self.new(obj)
         except FileNotFoundError:
             pass
-
-# Create a unique FileStorage instance for the application
-storage = FileStorage()
-storage.reload()
