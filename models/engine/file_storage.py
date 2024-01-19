dbfbf80d4c90603aab51be98ca119fe94abcf72e
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 21:17:07 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
 
 import json
+from models.base_model import BaseModel
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file."""
         data = {}
         for key, obj in self.__objects.items():
             data[key] = obj.to_dict()
         with open(self.__file_path, 'w') as file:
             json.dump(data, file)
 
+    def reload(self):
         try:
             with open(self.__file_path, 'r') as file:
                 data = json.load(file)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    # Move the import statement to the reload method
-                    from models.base_model import BaseModel
                     cls = globals()[class_name]
                     obj = cls(**value)
                     self.new(obj)
