2e04c438f18fdd53d685c0d45fada69ebe8d1fa2
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 22:31:18 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -13,3 +13,11 @@ classes = {
 storage = FileStorage()
 storage.reload()
 
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
