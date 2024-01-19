1ca02ae48d702e21c20faec14538199fa3c19f56
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 22:23:43 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+
 from models.engine.file_storage import FileStorage
+
+classes = {
+    'BaseModel': BaseModel,  # Add other classes as needed
+    'User': User,
+    'State': State,
+    # Add more classes as needed
+}
 
 storage = FileStorage()
 storage.reload()
+
