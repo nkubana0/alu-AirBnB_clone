10de4e5a97938cba7a94e2a5eec08cd137a060fa
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 21:54:35 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# models/state.py
+
 from models.base_model import BaseModel
 
 class State(BaseModel):
@@ -8,18 +10,22 @@ class State(BaseModel):
     def __init__(self, *args, **kwargs):
         """Initialize State instance."""
         super().__init__(*args, **kwargs)
+        # Add any additional attributes specific to the State class here
 
     def to_dict(self):
         """Return a dictionary representation of the State object."""
         obj_dict = super().to_dict()
+        # Add any specific attributes from State class to the dictionary here
         return obj_dict
+
+    # Add any specific methods for the State class here
 
     @property
     def id(self):
         """Getter method for the 'id' attribute."""
-        return super().id
+        return self._id
 
     @id.setter
     def id(self, value):
         """Setter method for the 'id' attribute."""
-        super(State, type(self)).id.fset(self, value)
+        self._id = value
