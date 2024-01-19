aa6d866ffc59c51eb4e63b07e444dbaee5509263
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 22:36:24 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State]
+from models.state import State
 
 classes = {
     'BaseModel': BaseModel,
