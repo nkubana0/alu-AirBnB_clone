4325376848ed1af2065812a9765c906a30dc6dcc
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 22:02:56 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,17 +1,57 @@
 #!/usr/bin/python3
 
 from models.base_model import BaseModel
+from datetime import datetime
 
 class Review(BaseModel):
-    """Review class."""
-    def __init__(self, text="", user_id="", place_id="", **kwargs):
-        super().__init__(**kwargs)
-        self.text = text
-        self.user_id = user_id
-        self.place_id = place_id
+    """Review class that inherits from BaseModel."""
 
-    @classmethod
-    def from_dict(cls, obj_dict):
-        """Create a Review instance from a dictionary."""
-        review_instance = cls(**obj_dict)
-        return review_instance
+    def __init__(self, *args, **kwargs):
+        """Initialize Review instance."""
+        super().__init__(*args, **kwargs)
+        # Add any additional attributes specific to the Review class here
+        self.text = kwargs.get('text', '')  # Add any default value for the text attribute
+        # Set created_at and updated_at if not provided in kwargs
+        if 'created_at' not in kwargs:
+            self.created_at = datetime.utcnow()
+        if 'updated_at' not in kwargs:
+            self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """Return a dictionary representation of the Review object."""
+        obj_dict = super().to_dict()
+        # Add any specific attributes from Review class to the dictionary here
+        obj_dict['text'] = self.text
+        return obj_dict
+
+    # Add any specific methods for the Review class here
+
+    @property
+    def id(self):
+        """Getter method for the 'id' attribute."""
+        return self.__dict__.get('id')
+
+    @id.setter
+    def id(self, value):
+        """Setter method for the 'id' attribute."""
+        self.__dict__['id'] = value
+
+    @property
+    def created_at(self):
+        """Getter method for the 'created_at' attribute."""
+        return self.__dict__.get('created_at')
+
+    @created_at.setter
+    def created_at(self, value):
+        """Setter method for the 'created_at' attribute."""
+        self.__dict__['created_at'] = value
+
+    @property
+    def updated_at(self):
+        """Getter method for the 'updated_at' attribute."""
+        return self.__dict__.get('updated_at')
+
+    @updated_at.setter
+    def updated_at(self, value):
+        """Setter method for the 'updated_at' attribute."""
+        self.__dict__['updated_at'] = value
