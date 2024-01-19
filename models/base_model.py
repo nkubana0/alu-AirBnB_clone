4a7039fdeb345042661ec48c591a2431efaa9c8e
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 14:56:37 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Module for BaseModel class"""
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """Defines the BaseModel class"""
@@ -19,6 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return string representation of the instance"""
@@ -28,7 +30,7 @@ class BaseModel:
     def save(self):
         """Update updated_at attribute"""
         self.updated_at = datetime.now()
-
+        storage.save()
 
     def to_dict(self):
         """Return dictionary representation of the instance"""
