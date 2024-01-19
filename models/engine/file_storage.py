764d6e42e6b02cf521d8f2f26766bf69b508abd8
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-19 20:11:34 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
-"""Module for FileStorage class"""
 import json
-from models.base_model import BaseModel
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances"""
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w', encoding='utf-8') as file:
-            json.dump(obj_dict, file)
+        data = {}
+        for key, obj in self.__objects.items():
+            data[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(data, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as file:
-                obj_dict = json.load(file)
-                for key, value in obj_dict.items():
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    class_module = __import__('models.' + class_name, fromlist=[class_name])
-                    obj_instance = getattr(class_module, class_name)(**value)
-                    self.__objects[key] = obj_instance
+                    cls = globals()[class_name]
+                    obj = cls(**value)
+                    self.new(obj)
         except FileNotFoundError:
             pass
-
-storage = FileStorage()
-storage.reload()
