7f25dfdd6d9df2be3abef4b8cd836ae7f712d2cd
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-20 14:08:03 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,9 @@
-#!/usr/bin/python3
-
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-
-classes = {
-    'BaseModel': BaseModel,
-    'User': User,
-    'State': State,
-}
 
 storage = FileStorage()
 storage.reload()
 
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.review import Review
