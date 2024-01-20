2a69527a688d438ff1b4932fc11500dbf4cc8cdd
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-20 14:05:41 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ import uuid
 
 class BaseModel:
     def __init__(self, **kwargs):
+        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -18,6 +19,7 @@ class BaseModel:
             else:
                 setattr(self, key, value)
     def to_dict(self):
+        from models import storage
         obj_dict = self.__dict__.copy()
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
@@ -25,6 +27,7 @@ class BaseModel:
         return obj_dict
     
     def __str__(self):
+        from models import storage
         class_name = self.__class__.__name__
         created_at_str = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         updated_at_str = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
