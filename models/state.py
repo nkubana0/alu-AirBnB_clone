391fc1cdbd519eaafbecd4ae52e8743dd21e9de7
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-20 20:11:33 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
+"""
+This module contains the State class (Blueprint for creating State objects).
+"""
 
 from models.base_model import BaseModel
 
+
 class State(BaseModel):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.name = kwargs.get('name', '')
+    """
+    This is the state class
 
-    def to_dict(self):
-        """Return a dictionary representation of the object."""
-        obj_dict = super().to_dict()
-        obj_dict['name'] = self.name
-        return obj_dict
+    Attributes:
+        name (str): The name of the state
+    """
+    name = ""
 
-    @classmethod
-    def from_dict(cls, obj_dict):
-        """Create a new instance from a dictionary."""
-        if obj_dict is None or not isinstance(obj_dict, dict):
-            return None
-
-        state = cls()
-        state.name = obj_dict.get('name', '')
-        return state
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.name = State.name
