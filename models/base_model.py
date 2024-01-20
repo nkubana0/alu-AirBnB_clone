b7ea6b7c700922ee7acc77ec6a5a26be47a6b8fb
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-20 19:29:13 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,61 @@
 #!/usr/bin/python3
+"""
+This module defines a BaseModel class that
+defines all common attributes/methods for model classes
+"""
 
-from datetime import datetime
 import uuid
-from models import storage  # Import the storage variable
+import models
+from datetime import datetime
+
 
 class BaseModel:
-    def __init__(self, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """
+    This is the base model class.
+    """
 
-        for key, value in kwargs.items():
-            if key in ["created_at", "updated_at"]:
-                if isinstance(value, str):
-                    setattr(self, key, datetime.fromisoformat(value))
-                else:
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize public instance attributes.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                if key != '__class__':
                     setattr(self, key, value)
-            else:
-                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
-        if not kwargs:  # If it's a new instance, call new(self) on storage
-            storage.new(self)
+    def save(self):
+        """
+        Updates the file storage with the new/updated information.
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        obj_dict["__class__"] = self.__class__.__name__
-        return obj_dict
-    
-    def __str__(self):
-        class_name = self.__class__.__name__
-        created_at_str = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        updated_at_str = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return "[{}] ({}) {{'id': '{}', 'created_at': '{}', 'updated_at': '{}'}}".format(
-            class_name, self.id, self.id, created_at_str, updated_at_str
-        )
-    
-    def save(self):
-        """Save the current instance to the JSON file."""
-        storage.save()  # Call save(self) method of storage
+        """
+        Convert the object to a dictionary representation.
 
-    def reload(self):
-        """Reload instances from the JSON file."""
-        storage.reload()
+        Returns:
+            dict: A dictionary representation of the object.
+        """
+
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                value = value.isoformat()
+            obj_dict[key] = value
+
+        return obj_dict
+
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel class.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
