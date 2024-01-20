d0b999c973d39a8aca3008a8aec94685787a5278
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-20 14:09:14 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,63 +1,8 @@
 #!/usr/bin/python3
 
 from models.base_model import BaseModel
-from datetime import datetime
 
 class State(BaseModel):
-    """State class that inherits from BaseModel."""
-    
-    def __init__(self, *args, **kwargs):
-        """Initialize State instance."""
-        super().__init__(*args, **kwargs)
-        # Add any additional attributes specific to the State class here
-        self.name = kwargs.get('name', '')  # Add any default value for the name attribute
-        # Set created_at and updated_at if not provided in kwargs
-        if 'created_at' not in kwargs:
-            self.created_at = datetime.utcnow()
-        if 'updated_at' not in kwargs:
-            self.updated_at = datetime.utcnow()
-
-    def to_dict(self):
-        """Return a dictionary representation of the State object."""
-        obj_dict = super().to_dict()
-        # Add any specific attributes from State class to the dictionary here
-        obj_dict['name'] = self.name
-        return obj_dict
-
-    @classmethod
-    def from_dict(cls, obj_dict):
-        """Create a new State instance from a dictionary."""
-        # Add any specific logic to create a State instance from the dictionary here
-        return cls(**obj_dict)
-
-    # Add any specific methods for the State class here
-
-    @property
-    def id(self):
-        """Getter method for the 'id' attribute."""
-        return self.__dict__.get('id')
-
-    @id.setter
-    def id(self, value):
-        """Setter method for the 'id' attribute."""
-        self.__dict__['id'] = value
-
-    @property
-    def created_at(self):
-        """Getter method for the 'created_at' attribute."""
-        return self.__dict__.get('created_at')
-
-    @created_at.setter
-    def created_at(self, value):
-        """Setter method for the 'created_at' attribute."""
-        self.__dict__['created_at'] = value
-
-    @property
-    def updated_at(self):
-        """Getter method for the 'updated_at' attribute."""
-        return self.__dict__.get('updated_at')
-
-    @updated_at.setter
-    def updated_at(self, value):
-        """Setter method for the 'updated_at' attribute."""
-        self.__dict__['updated_at'] = value
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = kwargs.get('name', '')
