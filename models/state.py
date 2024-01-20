b270d0d0bbee22f32f1bfc76943c349cf7418c96
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-20 14:17:16 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -6,3 +6,19 @@ class State(BaseModel):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.name = kwargs.get('name', '')
+
+    def to_dict(self):
+        """Return a dictionary representation of the object."""
+        obj_dict = super().to_dict()
+        obj_dict['name'] = self.name
+        return obj_dict
+
+    @classmethod
+    def from_dict(cls, obj_dict):
+        """Create a new instance from a dictionary."""
+        if obj_dict is None or not isinstance(obj_dict, dict):
+            return None
+
+        state = cls()
+        state.name = obj_dict.get('name', '')
+        return state
