f2053336af3ace6be809ba9d9f47796f904c10b3
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-20 19:22:27 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,27 +1,46 @@
 #!/usr/bin/python3
 
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
+from datetime import datetime
+import uuid
+from models import storage  # Import the storage variable
 
-# Dictionary mapping class names to class objects
-classes = {
-    'BaseModel': BaseModel,
-    'User': User,
-    'State': State,
-    'City': City,
-    'Place': Place,
-    'Amenity': Amenity,
-    'Review': Review,
-}
+class BaseModel:
+    def __init__(self, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-# Create a unique FileStorage instance for your application
-storage = FileStorage()
+        for key, value in kwargs.items():
+            if key in ["created_at", "updated_at"]:
+                if isinstance(value, str):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+            else:
+                setattr(self, key, value)
 
-# Call reload() method on this variable
-storage.reload()
+        if not kwargs:  # If it's a new instance, call new(self) on storage
+            storage.new(self)
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
+        return obj_dict
+    
+    def __str__(self):
+        class_name = self.__class__.__name__
+        created_at_str = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        updated_at_str = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        return "[{}] ({}) {{'id': '{}', 'created_at': '{}', 'updated_at': '{}'}}".format(
+            class_name, self.id, self.id, created_at_str, updated_at_str
+        )
+    
+    def save(self):
+        """Save the current instance to the JSON file."""
+        storage.save()  # Call save(self) method of storage
+
+    def reload(self):
+        """Reload instances from the JSON file."""
+        storage.reload()
