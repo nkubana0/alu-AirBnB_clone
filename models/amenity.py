21c733ff06ad839880f2573dbb4c918ce58d13c8
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-20 20:05:01 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
+"""
+This module contains Amenity class (Blueprint for creating Amenity objects).
+"""
 
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
-    """Amenity class."""
-    def __init__(self, name="", **kwargs):
-        super().__init__(**kwargs)
-        self.name = name
+    """
+    This is the amenity class
 
-    def to_dict(self):
-        """Return a dictionary representation of the Amenity instance."""
-        amenity_dict = super().to_dict()
-        amenity_dict['name'] = self.name
-        return amenity_dict
+    Attributes:
+        name (str): The name of the amenity
+    """
+    name = ""
 
-    # Add any additional methods or properties as needed
-
-    @classmethod
-    def from_dict(cls, obj_dict):
-        """Create an Amenity instance from a dictionary."""
-        amenity_instance = cls(**obj_dict)
-        return amenity_instance
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
