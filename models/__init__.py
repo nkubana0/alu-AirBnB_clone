4f2830f88e293bf1c5dae07e1fad8771afb33014
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-01-20 19:28:40 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-
-from models.engine.file_storage import FileStorage
+"""
+This module serves as the initialization file for the models package.
+"""
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from models.user import User
 from models.state import State
 from models.city import City
-from models.place import Place
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
 
-# Dictionary mapping class names to class objects
-classes = {
-    'BaseModel': BaseModel,
-    'User': User,
-    'State': State,
-    'City': City,
-    'Place': Place,
-    'Amenity': Amenity,
-    'Review': Review,
-}
 
-# Create a unique FileStorage instance for your application
 storage = FileStorage()
-
-# Call reload() method on this variable
 storage.reload()
+loaded_objects = storage.all()
+
+
+classes = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
+}
